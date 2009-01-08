b556e49dba33309b3b0c20c9c0995025f7fef37f
Joan Marc Carbo Arnau <jmcarbo@jmca-MBP.local>
2009-01-08 20:04:05 +0100

    Removed pkg directory

--- pkg/imapstore-0.0.1/lib/imapstore.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'imapstore/imapstore.rb'
-
-module IMAPSTORE
-  VERSION = '0.0.1'
-
-end
