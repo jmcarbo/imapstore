7217f4ddb18cd442179549450f9840c9525a1ac7
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2009-01-08 09:44:02 +0100

    Added pkg dir

---
+++ pkg/imapstore-0.0.1/lib/imapstore.rb
@@ -0,0 +1,9 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'imapstore/imapstore.rb'
+
+module IMAPSTORE
+  VERSION = '0.0.1'
+
+end
